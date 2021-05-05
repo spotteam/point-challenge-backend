d60baaf7e41a458be502e712cdde7f320f44ee5d
eswardhinak <eswardhinak@gmail.com>
2021-05-04 19:11:55 -0700

    Initial commit with migrations. Nothing's really connected yet

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/', function(req, res){
+  res.json({"message": "root for point-app"})
+})
+
+app.listen(3000, function() { console.log('Node server listening on port 3000')})
