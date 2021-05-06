8fbb4fef54ec1452a9005ec52b4f9d4774f37fa7
eswardhinak <eswardhinak@gmail.com>
2021-05-06 15:18:36 -0700

    removed profile endpoint

--- routes/secure-routes.js
+++ routes/secure-routes.js
@@ -103,15 +103,5 @@ router.post(
   })   
 );
 
-router.get(
-  '/profile',
-  (req, res, next) => {
-    res.json({
-      message: 'You made it to the secure route',
-      user: req.user,
-      token: req.query.secret_token
-    })
-  }
-)
 
 module.exports = router;
